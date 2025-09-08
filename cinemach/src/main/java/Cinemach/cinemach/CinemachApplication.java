87762ec3eefd04612d0a3732de435a64b78ed605
joao <frazaosilva90@gmail.com>
2025-09-08 15:17:05 -0300

    criaçao do projeto spring

---
+++ cinemach/src/main/java/Cinemach/cinemach/CinemachApplication.java
@@ -0,0 +1,13 @@
+package Cinemach.cinemach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemachApplication.class, args);
+	}
+
+}
---
+++ cinemach/src/test/java/Cinemach/cinemach/CinemachApplicationTests.java
@@ -0,0 +1,13 @@
+package Cinemach.cinemach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
